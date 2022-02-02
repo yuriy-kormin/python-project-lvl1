5377fa9540fb49595c41019ae9c6cec39da1c130
yuriy-kormin <yuriy.kormin@gmail.com>
2022-02-03 00:16:03 +0300

    step3

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
+from cli import welcome_user as welcome_user
 
 def main():
     print ("Welcome to the Brain Games!")
-
+    name=welcome_user()
+    print ("Hello, %s!" % name)
 if __name__ == '__main__':
     main()
---
+++ brain_games/scripts/cli.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+import prompt
+
+def welcome_user():
+    return prompt.string(empty=False,prompt='May I have your name? ')
+
