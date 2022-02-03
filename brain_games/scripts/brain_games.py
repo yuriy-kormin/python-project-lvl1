98b6d5eff9556dc052f467c703608613d8e3911b
yuriy-kormin <yuriy.kormin@gmail.com>
2022-02-03 13:40:54 +0300

    add my new workflow

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 from cli import welcome_user as welcome_user
 
+
 def main():
-    print ("Welcome to the Brain Games!")
-    name=welcome_user()
-    print ("Hello, %s!" % name)
+    print("Welcome to the Brain Games!")
+    name = welcome_user()
+    print("Hello, %s!" % name)
+
+
 if __name__ == '__main__':
     main()
--- brain_games/scripts/cli.py
+++ brain_games/scripts/cli.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import prompt
 
-def welcome_user():
-    return prompt.string(empty=False,prompt='May I have your name? ')
 
+def welcome_user():
+    return prompt.string(empty=False, prompt='May I have your name? ')
