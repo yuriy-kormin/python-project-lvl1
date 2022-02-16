bf1e62ae2fd3c28474c2c86b4f4fdf506b1860c4
yuriy.kormin <yuriy.kormin@gmail.com>
2022-02-16 20:26:02 +0300

    fix lint

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -2,13 +2,14 @@
 from random import randint
 
 
-RULES_PRIME = "Answer \"yes\" if given number is prime. Otherwise answer \"no\"."
+RULES_PRIME = "Answer \"yes\" if given number is prime. "
+RULES_PRIME += "Otherwise answer \"no\"."
 
 
 def prime():
     ''' Generate digit and returm is it prime '''
-    digit = randint (1,50)
-    answer  = 'yes'
+    digit = randint(1, 50)
+    answer = 'yes'
     if digit != 1:
         for i in range(2, digit):
             if not digit % i:
--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -2,8 +2,8 @@
 from brain_games.games.prime import prime, RULES_PRIME
 from brain_games.games_logic import engine
 
+
 def main():
-    
     engine(prime, RULES_PRIME)
 
 
