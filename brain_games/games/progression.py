03d110f6e00ea5a8406e2b735b314a811eee1375
yuriy.kormin <yuriy.kormin@gmail.com>
2022-02-18 14:58:37 +0300

    fix progression logic

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -11,12 +11,13 @@ def get_rules():
 def get_question_answer():
     '''Generate arithmetic progression with missing member'''
     start_digit = randint(2, 9)
-    step = randint(2, 9)
+    step = randint(1, 9)
     length = randint(5, 10)
-    end_digit = start_digit + (step - 1) * length + 1
-    progression = [i for i in (range(start_digit, end_digit, step))]
-    missing_member_index = progression.index(choice(progression))
-    answer = str(progression[missing_member_index])
+    end_digit = start_digit + step * (length - 1)
+    progression = [i for i in (range(start_digit, end_digit+1, step))]
+    missing_member = choice(progression)
+    missing_member_index = progression.index(missing_member)
     progression[missing_member_index] = '..'
+    answer = missing_member
     question = " ".join([str(elem) for elem in progression])
     return question, answer
