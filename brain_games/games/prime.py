148ff64c2dbec67fd732aa21eb8df47555d5c5b4
yuriy.kormin <yuriy.kormin@gmail.com>
2022-02-18 14:32:15 +0300

    fix prime logic

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -12,9 +12,13 @@ def get_rules():
 def get_question_answer():
     ''' Generate digit and returm is it prime '''
     digit = randint(1, 50)
-    answer = 'yes'
-    if digit != 1:
+    is_not_prime = False
+    if digit > 1:
         for i in range(2, digit):
             if not digit % i:
-                answer = 'no'
+                is_not_prime = True
+                break
+    else:
+        is_not_prime = True
+    answer = 'no' if is_not_prime else 'yes'
     return digit, answer
